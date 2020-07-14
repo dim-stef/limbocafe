86a19c8c3065fbecf6bcb89764346222b6340ad6
dim <jimstef@outlook.com>
2020-07-14 23:18:25 +0300

    image fix

--- src/components/FadeImage/FadeImage.js
+++ src/components/FadeImage/FadeImage.js
@@ -1,41 +1,37 @@
 import React, { useEffect, useRef, useState } from "react";
 
-const FadeImage = React.memo(
-  function FadeImage(props) {
-    const ref = useRef(null);
-    const [loaded, setLoaded] = useState(false);
+function FadeImage(props) {
+  const ref = useRef(null);
+  const [loaded, setLoaded] = useState(false);
 
-    let style = props.style ? props.style : {};
+  let style = props.style ? props.style : {};
 
-    function onImageLoad() {
-      setLoaded(true);
-    }
-
-    return (
-      <img
-        alt={props.alt}
-        ref={ref}
-        {...props}
-        style={
-          loaded
-            ? {
-                transition: "opacity 0.3s",
-                opacity: 1,
-                ...style,
-              }
-            : {
-                opacity: 0,
-                transition: "opacity 0.3s",
-                ...style,
-              }
-        }
-        onLoad={onImageLoad}
-      />
-    );
-  },
-  (prevProps, nextProps) => {
-    return prevProps.src === nextProps.src;
+  function onImageLoad() {
+    setLoaded(true);
   }
-);
+
+  return (
+    <img
+      alt={props.alt}
+      ref={ref}
+      {...props}
+      style={
+        loaded
+          ? {
+              transition: "opacity 0.3s",
+              opacity: 1,
+              ...style,
+            }
+          : {
+              opacity: 0,
+              transition: "opacity 0.3s",
+              ...style,
+            }
+      }
+      onLoad={onImageLoad}
+    />
+  );
+}
+
 
 export default FadeImage;
